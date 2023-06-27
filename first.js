150b7dee7a056c35b2dbfdcaaede378b99df82d7
정현규 <gusrb0511@gmail.com>
2023-06-28 00:42:37 +0900

    0627

---
+++ first.js
@@ -0,0 +1,5 @@
+"use strict";
+let a = 'hello';
+a = 1234;
+// a를 string으로 할당했기에 에러
+console.log(a);
--- first.ts
+++ first.ts
@@ -1,4 +1,37 @@
-let a: string = 'hello';
-a = 1234;
-// a를 string으로 할당했기에 에러
+const a: string = '5';
+// 변수 이름 뒤, 소문자여야함.
+const b: number = 5;
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const f: any = 123; //모든 타입, 하지만 any를 쓰면 타입스크립트의 의미가 없음
+// const f: symbol = Symbol.for('abc');
+// const g: bigint = 1000000n;
 
+
+//function add(x: number, y: number): number { return x + y } //타입을 붙여주는것을 타이핑이라고 함
+//리턴값은 매개변수 자리 바로 뒤
+
+// const add: (x: number, y: number) => number = (x, y) => x + y;
+// 타입부분을 지웠을때도 자바스크립트적으론 말이 되야함.
+
+// type Add = (x: number, y: number) => number;
+// const add: Add = (x, y) => x + y;
+// type으로 타입을 선언하는 방식 타입 애일리어스 (type alias)
+
+// interface Add{
+//     (x: number, y:number): number;
+// }
+// const add: Add = (x, y) => x + y;
+// 인터페이스 방식
+
+// const arr: string[] = ['123', '456']
+// const arr2: number[] = [123, 456]
+// const arr2: Array<number> = [123, 456]
+// 배열; 꺽쇄, 제네릭
+// const arr3: [number,number,string] = [123, 456,'hello'];
+// 튜플;
+// const b: 5 = 5;
+// 이런 식으로 정확한 원시값 타입도 존재
+ 
+// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
